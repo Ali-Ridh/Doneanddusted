9bc570517ccec60edda0bd400d3ced093348e965
Ali-Ridh <alibbbshd@gmail.com>
2025-11-30 18:08:41 +0700

    nasi goreng sego lele

--- main.go
+++ main.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	steam "github.com/Philipp15b/go-steamapi"
-)
-
-func main() {
-	apiKey := os.Getenv("STEAM_API_KEY")
-	if apiKey == "" {
-		log.Fatal("STEAM_API_KEY environment variable not set")
-	}
-
-	client := &http.Client{}
-	api := steam.New(apiKey, client)
-
-	apps, err := api.ISteamApps().GetAppList()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("List of Steam Apps:")
-	for _, app := range apps.Apps {
-		fmt.Printf("%d: %s\n", app.AppID, app.Name)
-	}
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
 }
--- server.go
+++ server.go
@@ -8,18 +8,52 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"gorm.io/driver/sqlite"
 )
+
+type User struct {
+	ID        uint   `gorm:"primaryKey"`
+	Username  string `gorm:"unique;not null"`
+	Password  string `gorm:"not null"`
+	Email     string `gorm:"unique;not null"`
+	CreatedAt time.Time
+}
 
 func main() {
 	router := gin.Default()
-	quit := make(chan bool)
+
+	db, err := gorm.Open(sqlite.Open("forum.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+
+	// AutoMigrate the schema
+	db.AutoMigrate(&User{})
 
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Welcome to the forum app!")
 	})
 
 	router.POST("/register", func(c *gin.Context) {
-		c.String(http.StatusOK, "Register endpoint")
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// TODO: Validate user data
+
+		// TODO: Hash password
+
+		// TODO: Save user to database
+		result := db.Create(&user)
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create user"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Registration successful"})
 	})
 
 	router.POST("/login", func(c *gin.Context) {
@@ -42,8 +76,14 @@ func main() {
 		}
 	}()
 
+	quit := make(chan bool)
+	router.POST("/shutdown", func(c *gin.Context) {
+		c.String(http.StatusOK, "Shutting down server")
+		log.Println("Shutting down server...")
+		close(quit)
+	})
+
 	<-quit
-	log.Println("Shutting down server...")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
